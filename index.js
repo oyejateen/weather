f8448dc20689eb2d3ff45761f36caf1cf0bbaf06
Jatin <75193966+TEDDY6969@users.noreply.github.com>
2022-11-21 14:32:44 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { render } from "react-snapshot";
+
+import { App } from "./src/app/App";
+
+import "./src/index.css";
+
+render(<App />, document.getElementById("root"));
