f9770e43f8de14d32bf51d206eb307f0b90db3b7
Jatin <75193966+TEDDY6969@users.noreply.github.com>
2022-11-21 15:46:58 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { render } from "react-snapshot";
-
-import { App } from "./src/app/App";
-
-import "./src/index.css";
-
-render(<App />, document.getElementById("root"));
